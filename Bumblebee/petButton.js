9f1827edff63a73a0b0e947faf5ed51e173e8f52
YungLust <artyr.yanchuk2@gmail.com>
2024-11-23 20:15:46 +0200

    Now button on click adds cute emojis to the paragraph

--- Bumblebee/petButton.js
+++ Bumblebee/petButton.js
@@ -1,22 +1,43 @@
 const outputField = document.querySelector('.outputField');
 const btn = document.querySelector('.petBtn');
-console.log(outputField,btn);
+const maxSymbols = 13
 
 let newText = "";
 
 btn.addEventListener('click', function(event){
     //currText = outputField.textContent;
     
-    if (newText.length < 30 ){
+    if (newText.length < maxSymbols*2 ){
         newText += getRandomSymbol();
     }
     else{
-        newText = ""
+        newText = " "
     }
     outputField.textContent = newText;
 })
 
 function getRandomSymbol() {
-    let symbol = Math.floor(Math.random()*10);
+    let chance = Math.floor(Math.random()*100);
+    let symbol = "";
+    if (chance < 32){ // 1/3
+        symbol = "❤️"
+    }
+    else if (chance < 65){ // 1/3
+        symbol = "🥰"
+    }
+    else if (chance < 98){ // 1/3
+        symbol = "🐝"
+    }
+    else{ // 1/100
+        symbol = "☀️";
+    }
+
     return symbol;
 }
+
+/* todo: 
+ cool animation while clicking on button
+ cool footer with my nickname
+ cool cool popup when clicking on my nickname in footer
+
+*/
