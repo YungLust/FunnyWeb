57644b167301c0eb870cf1fe1192dc9f28a295ff
YungLust <artyr.yanchuk2@gmail.com>
2024-11-25 17:23:12 +0200

    Changed logic of random symbol. Added new emojis.

--- Bumblebee/petButton.js
+++ Bumblebee/petButton.js
@@ -3,41 +3,42 @@ const btn = document.querySelector('.petBtn');
 const maxSymbols = 13
 
 let newText = "";
+const emojis = ["❤️","🥰", "🐝", "☀️", "🤗", "🩷"];
+const secretPhrases = ["KY$", "404", "?", "K̷̨͖̣̰̠̝̬̝͔̲͖̝̜̩̤̟͙̙̘̥̻̩͔͓̺̲͙̱̬̭͖̪͕̩̺̟̠̪̪͈͙̙̪̣̼̻̐̀̈́̓̂͋̀̂̍̉̍͂̓͗́̀̉̊̕͘͠ͅY̷̨̢̡̛̻̮̼̖͙̥͓̜̩͖̖̬͓̖̞̩̖͈͙͖̭̩̩͖̱͇͚̳͉̠̮̙̪̗̫͚͐̊̃̆̾̃̑̀͆̈́̆̅͑̉̓̅̀̅͗̊͗̀̚͝ͅͅ$̷̨̛̛̫̟̺͙͙̪̠͓̦̦͎̗̟̼̰͈̑̇̂͆̑̈́̿̂̉̅̈́͒͗͋̀̄̄̆̊̑̍̂̂̄́̄̌̕͘̕̚̕͜͠͝"]
 
-btn.addEventListener('click', function(event){
+btn.addEventListener('click', async function(event){
     //currText = outputField.textContent;
-    
-    if (newText.length < maxSymbols*2 ){
+    let secret = Math.floor(Math.random()*100);
+    console.log(secret);
+    if (newText.length < maxSymbols*2){
         newText += getRandomSymbol();
     }
     else{
-        newText = " "
+        newText = "";
     }
+    if (secret==1){
+        random = Math.floor(Math.random()*secretPhrases.length);
+        newText+=secretPhrases[random];
+    }
+    outputField.textContent = newText;
+    await sleep(25);
+    newText=newText.replace(/[!-@0-9a-zA-Zа-яА-Я]/g, "");
     outputField.textContent = newText;
 })
 
 function getRandomSymbol() {
-    let chance = Math.floor(Math.random()*100);
-    let symbol = "";
-    if (chance < 32){ // 1/3
-        symbol = "❤️"
-    }
-    else if (chance < 65){ // 1/3
-        symbol = "🥰"
-    }
-    else if (chance < 98){ // 1/3
-        symbol = "🐝"
-    }
-    else{ // 1/100
-        symbol = "☀️";
-    }
-
+    let random = Math.floor(Math.random()*emojis.length)
+    let symbol = emojis[random];  
     return symbol;
+}
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms)); //magic code
 }
 
 /* todo: 
  cool animation while clicking on button
- cool footer with my nickname
  cool cool popup when clicking on my nickname in footer
-
+ cool counter at the bottom right footer or in front of the button 
 */
