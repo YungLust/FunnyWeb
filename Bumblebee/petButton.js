b2360c1217e60d6e5a02ece56a23509bf9c79733
YungLust <artyr.yanchuk2@gmail.com>
2024-11-29 21:57:27 +0200

    Added chance variable to define chances of secret symbols

--- Bumblebee/petButton.js
+++ Bumblebee/petButton.js
@@ -3,18 +3,19 @@ const counterField = document.querySelector('.counterField');
 const btn = document.querySelector('.petBtn');
 const maxSymbols = 13;
 
-const emojis = ["❤️","🥰", "🐝", "☀️", "🤗", "🩷"];
+const emojis = ["❤️","🥰", "🐝", "🤗", "🩷", "☀️"];
 const secretPhrases = ["KY$", "404", "?", "K̷̨͖̣̰̠̝̬̝͔̲͖̝̜̩̤̟͙̙̘̥̻̩͔͓̺̲͙̱̬̭͖̪͕̩̺̟̠̪̪͈͙̙̪̣̼̻̐̀̈́̓̂͋̀̂̍̉̍͂̓͗́̀̉̊̕͘͠ͅY̷̨̢̡̛̻̮̼̖͙̥͓̜̩͖̖̬͓̖̞̩̖͈͙͖̭̩̩͖̱͇͚̳͉̠̮̙̪̗̫͚͐̊̃̆̾̃̑̀͆̈́̆̅͑̉̓̅̀̅͗̊͗̀̚͝ͅͅ$̷̨̛̛̫̟̺͙͙̪̠͓̦̦͎̗̟̼̰͈̑̇̂͆̑̈́̿̂̉̅̈́͒͗͋̀̄̄̆̊̑̍̂̂̄́̄̌̕͘̕̚̕͜͠͝"]
 
 let newText = "";
 let counter = retrieve();
 changeCounter();
+let chance = 100;
 
 btn.addEventListener('click', async function(event){
     counter++;
-    let secret = Math.floor(Math.random()*100);
+    let secret = Math.floor(Math.random()*2);
     
-    if (newText.length < maxSymbols*2){
+    if (newText.length < maxSymbols*chance){
         newText += getRandomSymbol();
     }
     else{
@@ -25,8 +26,10 @@ btn.addEventListener('click', async function(event){
         newText+=secretPhrases[random];
     }
     outputField.textContent = newText;
-    await sleep(25);
-    newText=newText.replace(/[!-@0-9a-zA-Zа-яА-Я]/g, "");
+    await sleep(525);
+    newText = newText.replace(/[!-@0-9a-zA-Zа-яА-Я]/g, ""); // /g is for every occurance in newText, not only the 1st one 
+
+    
     outputField.textContent = newText;
     changeCounter();
     
@@ -74,5 +77,4 @@ function retrieve(){
 /* todo: 
  cool animation while clicking on button
  cool cool popup when clicking on my nickname in footer
- cool saving of counter variable in cache
 */
